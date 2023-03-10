38c5389bb6b3cc1c5d7d524439dfa123b0cbe5a6
ChristopherWijnstekers <82508602+ChristopherWijnstekers@users.noreply.github.com>
2023-03-10 01:13:03 +0000

    added info pass through to accordions in app menus

--- src/About.js
+++ src/About.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import "./Styles.css"
+import GenAccordion from "./accordionElement"
 
 //data passthrough
 import { useLocation } from "react-router-dom"
@@ -10,26 +11,38 @@ function About(props) {
   const data = location.state
 
   return (
+
     <div className="body">
       <div className="header-nav-bar">
         <div className="back-btn">
           <Link to="../">Back</Link>
         </div>
+        <div className="appTitle">
+          <p>{data.title}</p>
+        </div>
       </div>
-      <div className="appTitle">
-        <p>{data["title"]}</p>
-      </div>
-      <div className="appInfo1">
-        <p>{data["info1"]}</p>
-      </div>
-      <div className="appInfo1">
-        <p>{data["info2"]}</p>
-      </div>
-      <div className="appInfo1">
-        <p>{data["info3"]}</p>
-      </div>
-      <div className="appInfo1">
-        <p>{data["info4"]}</p>
+
+      <div className="accordion">
+        <GenAccordion
+          title= "The information they have on you"text={data["info1"]}
+        ></GenAccordion>
+
+        <GenAccordion
+          title= "Privacy protection tutorial"text={data["info2"]}
+        ></GenAccordion>
+
+        <GenAccordion
+          title= "Misleading UI they use"text={data["info3"]}
+        ></GenAccordion>
+
+        <GenAccordion
+          title= "their security test results"text={data["info4"]}
+        ></GenAccordion>
+
+        <GenAccordion
+          title= "Their terms and conditions"text={data["info5"]}
+        ></GenAccordion>
+
       </div>
     </div>
   )
--- src/accordionElement.js
+++ src/accordionElement.js
@@ -1,6 +1,18 @@
 import { React, useState } from "react"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material"
+import Grid from '@mui/material/Grid';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+
+function ChangeCheckBox (status){
+  const isExpanded = status.isExpanded;
+  if (isExpanded){
+    return (CheckBoxIcon)
+  }
+  return (CheckBoxOutlineBlankIcon)
+}
 
 const GenAccordion = ({ title, text }) => {
   const [expanded, setExpanded] = useState(false)
@@ -8,7 +20,7 @@ const GenAccordion = ({ title, text }) => {
     setExpanded(isExpanded ? panel : false)
   }
 
-  return (
+return (
     <div>
       <Accordion
         expanded={expanded === "panel1"}
@@ -19,9 +31,18 @@ const GenAccordion = ({ title, text }) => {
           aria-controls="panel1-content"
           expandIcon={<ExpandMoreIcon />}
         >
-          <p>{title}</p>
+          <Grid item xs={1}>
+            <p>{title}</p>
+          </Grid>
+
+          <Grid item xs={2} >
+
+          </Grid>  
+
         </AccordionSummary>
+
         <AccordionDetails>{text}</AccordionDetails>
+        
       </Accordion>
     </div>
   )
